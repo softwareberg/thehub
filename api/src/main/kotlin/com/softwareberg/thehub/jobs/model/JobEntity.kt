a447eca76772fa2a08009591e0a23700bd5f7ea6
Michal Kowol <michal.kowol@vimn.com>
2018-10-11 23:43:00 +0200

    WIP removed unused imports

--- api/src/main/kotlin/com/softwareberg/thehub/jobs/model/JobEntity.kt
+++ api/src/main/kotlin/com/softwareberg/thehub/jobs/model/JobEntity.kt
@@ -4,7 +4,6 @@ import java.util.ArrayList
 import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.FetchType
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.JoinTable
