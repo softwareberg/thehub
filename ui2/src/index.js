529a26579aaac7f62f478d5f734c801353e0dc24
Jakub Romanowski <kotoko@users.noreply.github.com>
2019-07-24 13:44:37 +0200

    Copy old css

--- ui2/src/index.js
+++ ui2/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
+import './assets/css/main.scss';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 
