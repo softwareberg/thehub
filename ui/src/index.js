13ee60d5b10be842ccdd255d2fe04ed26c718bca
Jakub Romanowski <kotoko@users.noreply.github.com>
2019-07-29 10:53:56 +0200

    Rename directory

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './Components/App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
