816802601ec4cca7c7b72bb800f80ef85700348c
Jakub Romanowski <kotoko@users.noreply.github.com>
2019-07-31 13:07:07 +0200

    Add support for http 404

--- api/src/main/kotlin/eu/codeloop/thehub/errors/BasicErrorController.kt
+++ api/src/main/kotlin/eu/codeloop/thehub/errors/BasicErrorController.kt
@@ -2,6 +2,7 @@ package eu.codeloop.thehub.errors
 
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController
 import org.springframework.boot.web.servlet.error.ErrorAttributes
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,12 +12,17 @@ import javax.servlet.http.HttpServletRequest
 class BasicErrorController(errorAttributes: ErrorAttributes) : AbstractErrorController(errorAttributes) {
 
     @RequestMapping(PATH)
-    internal fun error(request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+    fun error(request: HttpServletRequest): ResponseEntity<ErrorResponse> {
         val errorAttributes = getErrorAttributes(request, getTraceParameter(request))
+        val path = errorAttributes.getOrDefault("path","").toString()
         val message = errorAttributes.getOrDefault("message", "").toString()
         val status = getStatus(request)
 
-        val error = ErrorResponse(status.value(), "GE", message)
+        val error = when(status) {
+            HttpStatus.NOT_FOUND -> ErrorResponse(status.value(), "NF", "$path not found.")
+            else -> ErrorResponse(status.value(), "GE", message)
+        }
+
         return ResponseEntity(error, status)
     }
 
