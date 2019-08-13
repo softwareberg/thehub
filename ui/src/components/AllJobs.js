30294f0d2366a3b451c95f0f9da82b852caea9de
Jakub Romanowski <kotoko@users.noreply.github.com>
2019-08-13 14:17:02 +0200

    Experimenting with saving page number (not working now)

--- ui/src/components/AllJobs.js
+++ ui/src/components/AllJobs.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import useAllJobsData from 'hooks/useAllJobsData';
 import useEmptyJobsDidMount from 'hooks/useEmptyJobsDidMount';
 import Job from 'components/Job';
 import Pagination from './Pagination';
+import setPage from '../redux/actions/setPage';
 
-const AllJobs = () => {
+const AllJobs = ({ history, match}) => {
   const jobs = useSelector(state => state.jobs);
   const pagination = useSelector(state => state.pagination);
+  const [isRestoredPage, setIsRestoredPage] = useState(false);
   const { page } = pagination;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (match.params.page !== pagination.page) {
+      if (!isRestoredPage) {
+        dispatch(setPage(match.params.page));
+      } else {
+        history.push(`/all/${pagination.page}`);
+      }
+    }
+    setIsRestoredPage(true);
+  }, [isRestoredPage, match.params.page, pagination.page, dispatch]);
 
   useEmptyJobsDidMount();
   useAllJobsData(page);
