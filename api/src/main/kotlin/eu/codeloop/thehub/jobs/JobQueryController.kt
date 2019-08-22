1138e985947f548d35fa0cdc637e188cdc3907ee
Jakub Romanowski <kotoko@users.noreply.github.com>
2019-08-22 09:25:41 +0200

    Fixes from review

--- api/src/main/kotlin/eu/codeloop/thehub/jobs/JobQueryController.kt
+++ api/src/main/kotlin/eu/codeloop/thehub/jobs/JobQueryController.kt
@@ -2,8 +2,8 @@ package eu.codeloop.thehub.jobs
 
 import eu.codeloop.thehub.base.PageResponse
 import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
-import org.springframework.data.web.PageableDefault
+import org.springframework.data.domain.Sort.Direction.ASC
+import org.springframework.data.domain.Sort.Direction.DESC
 import org.springframework.data.web.SortDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -18,10 +18,9 @@ class JobQueryController(private val jobQueryService: JobQueryService, private v
         @RequestParam keyword: String?,
         @RequestParam q: String?,
         @RequestParam hasStar: Boolean?,
-        @PageableDefault
         @SortDefault.SortDefaults(
-            SortDefault(sort = ["approvedAt"], direction = Sort.Direction.DESC),
-            SortDefault(sort = ["title"], direction = Sort.Direction.ASC)
+            SortDefault(sort = ["approvedAt"], direction = DESC),
+            SortDefault(sort = ["title"], direction = ASC)
         ) pageable: Pageable
     ): PageResponse<JobDto> {
         val jobs = jobQueryService.findAll(title, keyword, q, hasStar, pageable)
