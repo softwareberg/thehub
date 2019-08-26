0fbacc48a10363514473b9f7e4c5b577f42f5a8c
Jakub Romanowski <kotoko@users.noreply.github.com>
2019-08-26 13:01:48 +0200

    Refactor IntegrationTest to kotlin

--- api/src/it/kotlin/eu/codeloop/thehub/IntegrationTest.kt
+++ api/src/it/kotlin/eu/codeloop/thehub/IntegrationTest.kt
@@ -1,24 +1,24 @@
-package eu.codeloop.thehub;
+package eu.codeloop.thehub
 
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
 
-import static com.github.tomakehurst.wiremock.core.Options.DYNAMIC_PORT;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import com.github.tomakehurst.wiremock.core.Options.DYNAMIC_PORT
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringRunner::class)
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
 @AutoConfigureWireMock(port = DYNAMIC_PORT)
-public abstract class IntegrationTest {
+abstract class IntegrationTest {
 
     @Autowired
-    protected MockMvc mvc;
+    protected lateinit var mvc: MockMvc
 }
