ecf288a49aeb847d449e9cdd4e7b5c4903017606
Michal Kowol <michal.kowol@vimn.com>
2019-08-28 16:32:43 +0200

    Wiremock config update

--- api/src/it/kotlin/eu/codeloop/thehub/sync/FetchFromAPITest.kt
+++ api/src/it/kotlin/eu/codeloop/thehub/sync/FetchFromAPITest.kt
@@ -1,79 +1,49 @@
 package eu.codeloop.thehub.sync
 
 import com.github.tomakehurst.wiremock.client.WireMock.*
-import com.github.tomakehurst.wiremock.http.UniformDistribution
 import eu.codeloop.thehub.IntegrationTest
 import eu.codeloop.thehub.base.DatabaseSetup
 import eu.codeloop.thehub.base.DatabaseSetupOperations
 import org.awaitility.Awaitility.await
+import org.awaitility.core.ConditionFactory
 import org.junit.Test
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.MediaType.ALL_VALUE
-import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.MediaType.*
+import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import java.util.concurrent.TimeUnit.MILLISECONDS
+import wiremock.org.apache.http.HttpHeaders.CONTENT_TYPE
+import java.util.concurrent.TimeUnit.SECONDS
 
 class FetchFromAPITest : IntegrationTest() {
 
     @Autowired
     private lateinit var databaseSetup: DatabaseSetup
 
-    @Value("\${thehub.sync.domains}")
-    private lateinit var syncDomains: String
-
-    private val log = LoggerFactory.getLogger(FetchFromAPITest::class.java)
-
     @Test
     fun `it should fetch jobs from external api`() {
-        log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") //TODO - does not print :(
-
         // given
-        val lowerDelay = 200
-        val upperDealy = 2000
-        val awaitTime = ((upperDealy * 3) + (5 * 1000)).toLong() //download + delay after that
         databaseSetup.prepareDatabase(
             DatabaseSetupOperations.deleteAll()
         )
-        stubFor(get(urlPathEqualTo("/jobs")).willReturn(
-            aResponse()
-                .withStatus(200)
-                .withRandomDelay(UniformDistribution(lowerDelay, upperDealy))
-                .withHeader("Content-Type", "application/json; charset=utf-8")
-                .withBodyFile("body-api-jobs-yH61E.json") //TODO - is path OK?
-        ))
-        stubFor(get(urlPathEqualTo("/jobs/chief-financial-officer-cfo-1")).willReturn(
-            aResponse()
-                .withStatus(200)
-                .withRandomDelay(UniformDistribution(lowerDelay, upperDealy))
-                .withHeader("Content-Type", "application/json; charset=utf-8")
-                .withBodyFile("body-jobs-chief-financial-officer-cfo-1-kgwWj.txt") //TODO
-        ))
-        stubFor(get(urlPathEqualTo("/jobs/chief-marketing-officer-cmo-6")).willReturn(
-            aResponse()
-                .withStatus(200)
-                .withRandomDelay(UniformDistribution(lowerDelay, upperDealy))
-                .withHeader("Content-Type", "application/json; charset=utf-8")
-                .withBodyFile("body-jobs-chief-marketing-officer-cmo-6-8G7JB.txt") //TODO
-        ))
+        removeAllMappings()
+        addJsonMapping("/api/jobs?page=1", "body-api-jobs-yH61E.json")
+        addHtmlMapping("/jobs/chief-financial-officer-cfo-1", "body-jobs-chief-financial-officer-cfo-1-kgwWj.txt")
+        addHtmlMapping("/jobs/chief-marketing-officer-cmo-6", "body-jobs-chief-marketing-officer-cmo-6-8G7JB.txt")
+
         val syncRequest = MockMvcRequestBuilders
             .post("/api/jobs/sync")
-            .accept(ALL_VALUE)
+            .accept(APPLICATION_JSON_VALUE)
         val jobsRequest = MockMvcRequestBuilders
             .get("/api/jobs")
             .accept(APPLICATION_JSON_VALUE)
 
         // when
         val syncResult = mvc.perform(syncRequest)
-        await().atMost(awaitTime, MILLISECONDS).untilAsserted {
-            mvc.perform(jobsRequest)
-                .andExpect(status().isOk)
-                .andExpect(jsonPath("$.totalElements").value(2)) // TODO - works with 0 :)
+        val jobsResult = await().atMost(10, SECONDS).untilRequest({ mvc.perform(jobsRequest) }) { response ->
+            response.andExpect(jsonPath("$.totalElements").value(2))
         }
-        val jobsResult = mvc.perform(jobsRequest)
 
         // then
         syncResult
@@ -81,6 +51,34 @@ class FetchFromAPITest : IntegrationTest() {
         jobsResult
             .andExpect(status().isOk)
             .andExpect(jsonPath("$.totalElements").value(2))
-            // TODO - więcej asercji
+    }
+
+    private fun addJsonMapping(url: String, fileName: String) {
+        stubFor(get(url).willReturn(
+            aResponse()
+                .withStatus(200)
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
+                .withBodyFile(fileName)
+        ))
+    }
+
+    private fun addHtmlMapping(url: String, fileName: String) {
+        stubFor(get(url).willReturn(
+            aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", TEXT_HTML_VALUE)
+                .withBodyFile(fileName)
+        ))
+    }
+
+    fun ConditionFactory.untilRequest(resultProvider: () -> ResultActions, resultChecker: (ResultActions) -> Unit): ResultActions {
+        return this.until(resultProvider) { r ->
+            try {
+                resultChecker(r)
+                true
+            } catch (e: java.lang.AssertionError) {
+                false
+            }
+        }
     }
 }
